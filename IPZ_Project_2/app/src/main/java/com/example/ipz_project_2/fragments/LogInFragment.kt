d6a62208075d7936b258fe096b0ab20e46ae5072
Zenek <gracjanzeniuk@gmail.com>
2021-12-22 12:40:31 +0100

    login with otp handling

--- IPZ_Project_2/app/src/main/java/com/example/ipz_project_2/fragments/LogInFragment.kt
+++ IPZ_Project_2/app/src/main/java/com/example/ipz_project_2/fragments/LogInFragment.kt
@@ -1,29 +1,149 @@
 package com.example.ipz_project_2.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.ipz_project_2.R
+import com.example.ipz_project_2.databinding.FragmentLogInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.android.volley.Response
+import org.json.JSONObject
+import com.example.ipz_project_2.fragments.LogInFragmentDirections
 
+fun hashMailPassword(mail: String, password: String): String {
+    val to_encode = mail + password
+    val res = String(sha_256(to_encode).toByteArray())
+    Log.i("hash", res)
+    return res
+}
 
-class LogInFragment : Fragment() {
+class LogInFragment : Fragment(R.layout.fragment_log_in), View.OnClickListener {
+
+    private lateinit var loginButton: Button
+    private lateinit var binding: FragmentLogInBinding
+    private lateinit var navController: NavController
+    private lateinit var auth: FirebaseAuth
+    lateinit var mailPasswordHash : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.fragment_log_in, container, false)
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding = FragmentLogInBinding.bind(view)
+        navController = Navigation.findNavController(view)
 
+        loginButton = binding.loginButtonRegisterLogIn
+        loginButton.setOnClickListener(this)
     }
 
+    override fun onClick(v: View?) {
+        Log.d(TAG, "On click action")
+        when (v!!.id) {
+
+            loginButton.id -> LoginUser()
+
+        }
+    }
+
+    fun LoginUser() {
+        val email: String = binding.emailEdittextLogIn.text.toString().trim()
+        val password: String = binding.passwordEdittextLogIn.text.toString().trim()
+        Log.d(TAG, "Login button clicked")
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    getOTP(email, password)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        context, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+
+
+    private fun getOTP(mail: String, password: String) {
+        Log.i("getOTP", " Called getOTP")
+        val queue = Volley.newRequestQueue(this.activity)
+
+
+        val url = "http://zenek.pythonanywhere.com/token"
+        val params = HashMap<String, String>()
+        mailPasswordHash =  hashMailPassword(mail,password)
+        params["hashed"] = mailPasswordHash
+        params["mail"] = mail
+        Log.d("getOTP",params.toString())
+        val jsonObject = JSONObject(params as Map<*, *>?)
+        Log.d("getOTP",jsonObject.toString())
+        val request = JsonObjectRequest(Request.Method.POST,url,jsonObject,
+            { response ->
+                // Process the json
+                try {
+                    Log.d("getOTP", "Response received")
+                    Log.d("getOTP", response.toString())
+                    updateUI()
+                } catch (e: Exception) {
+                    Toast.makeText(
+                        context, "Something went wrong while sending otp request, Try again.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+            }, {response ->
+                Log.e("getOTP", "Something went wrong,")
+                val body = String(response.networkResponse.data)
+                Log.d("getOTP", body)
+                // Error in request
+//                textView.text = "Volley error: $it"
+                Toast.makeText(
+                    context, "Something went wrong while sending otp request, Try again.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            })
+        queue.add(request)
+    }
+
+    private fun updateUI() {
+        //TODO: change to fragment with otp code input
+        val action = LogInFragmentDirections.actionLogInFragmentToOtpcode(hash = mailPasswordHash)
+        navController.navigate(action)
+    }
+
+    companion object {
+        private const val TAG = "LOGIN"
+
+    }
 }
+
---
+++ IPZ_Project_2/app/src/main/java/com/example/ipz_project_2/otpcode.kt
@@ -0,0 +1,104 @@
+package com.example.ipz_project_2
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.ipz_project_2.databinding.FragmentLogInBinding
+import com.example.ipz_project_2.databinding.FragmentOtpcodeBinding
+import com.example.ipz_project_2.fragments.ChatFragmentArgs
+import com.example.ipz_project_2.fragments.LogInFragment
+import com.google.firebase.auth.FirebaseAuth
+import org.json.JSONObject
+import kotlin.math.log
+
+
+class otpcode : Fragment(), View.OnClickListener {
+
+
+    private lateinit var confirmButton: Button
+    private lateinit var codeText: EditText
+    private lateinit var binding: FragmentOtpcodeBinding
+    private lateinit var navController: NavController
+
+    private val args by navArgs<otpcodeArgs>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_otpcode, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentOtpcodeBinding.bind(view)
+        navController = Navigation.findNavController(view)
+
+        confirmButton = binding.otpButton
+        codeText = binding.editTextOTPCode
+        confirmButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            confirmButton.id -> verifyOTP()
+
+        }
+    }
+
+    private fun verifyOTP() {
+        Log.i("verifyOTP", " Called verifyOTP")
+        val queue = Volley.newRequestQueue(this.activity)
+        val url =
+            "http://zenek.pythonanywhere.com/token?otp=" + codeText.text.toString().trim() + "&hashed=" +
+                    args.hash
+
+        val request = StringRequest(
+            Request.Method.GET, url,
+            { response ->
+                // Process the json
+                try {
+                    UpdateUI()
+                } catch (e: Exception) {
+                    Toast.makeText(
+                        context, "Something went wrong while sending otp request, Try again.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+            }, {response->
+                Log.e("getOTP", "Something went wrong,")
+                val body = String(response.networkResponse.data)
+                Log.d("getOTP", body)
+                Toast.makeText(
+                    context, "Could not verify OTP code, try again",
+                    Toast.LENGTH_SHORT
+                ).show()
+            })
+        queue.add(request)
+    }
+
+    fun UpdateUI() {
+        Log.d("OTPCode","UpdateUI")
+
+        navController.navigate(R.id.action_otpcode_to_contactListFragment)
+    }
+
+    companion object {
+    }
+}
